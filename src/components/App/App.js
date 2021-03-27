08bddcc0cdf69fdcc3287ccfad27ca4058ce8827
IrinaKorbut <irisharomka96@gmail.com>
2021-03-27 13:12:14 +0300

    feat: implement structure main page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import Header from './../Header/Header'
+import Footer from './../Footer/Footer'
 import MainPage from './../MainPage/MainPage'
 import './App.css';
 
@@ -10,6 +11,7 @@ const App = () => {
       <Router>
         <Route exact path='/' component={MainPage} />
       </Router>
+      <Footer/>
     </div>
   );
 }
